7732cc54611f511f9633bac01ee9f40a83d291a8
Antonio Fabregat <fabregat.antonio@gmail.com>
2014-06-12 17:32:15 +0100

    minor change

--- src/main/java/org/reactome/web/elv/client/details/tabs/analysis/view/widgets/notfound/columns/ExpressionColumn.java
+++ src/main/java/org/reactome/web/elv/client/details/tabs/analysis/view/widgets/notfound/columns/ExpressionColumn.java
@@ -3,7 +3,6 @@ package org.reactome.web.elv.client.details.tabs.analysis.view.widgets.notfound.
 import com.google.gwt.cell.client.NumberCell;
 import com.google.gwt.i18n.client.NumberFormat;
 import org.reactome.web.elv.client.common.analysis.model.IdentifierSummary;
-import org.reactome.web.elv.client.common.analysis.model.PathwaySummary;
 
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
