f78fd8d08e9d661cf493e7469f6babd5ea16af11
Antonio Fabregat <fabregat.antonio@gmail.com>
2015-06-08 16:23:41 +0100

    REACT like ST_ID compatibility is meant to be kept for the moment

--- src/main/java/org/reactome/web/elv/client/manager/state/AdvancedState.java
+++ src/main/java/org/reactome/web/elv/client/manager/state/AdvancedState.java
@@ -318,7 +318,8 @@ public class AdvancedState implements StableIdentifierLoader.StableIdentifierLoa
             case CURATOR:
                 isSingleIdentifier = !value.contains("=");
             default:
-                isSingleIdentifier |= value.matches("^R-[A-Z]{3}-\\d+(\\.\\d+)?$") ;
+                //REACT like ST_ID compatibility is meant to be kept for a while ;)
+                isSingleIdentifier |= value.matches("^R-[A-Z]{3}-\\d+(\\.\\d+)?$") || value.matches("^REACT_\\d+(\\.\\d+)?$");
         }
         return isSingleIdentifier;
     }
